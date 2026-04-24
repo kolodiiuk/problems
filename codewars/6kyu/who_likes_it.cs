003ab52672852ce35d2d1d4689e805c53012f054
Nadia Kolodiiuk <nkolodeyuk@gmail.com>
2026-04-24 23:52:38 +0300

    add codewars problem

---
+++ codewars/6kyu/who_likes_it.cs
@@ -0,0 +1,32 @@
+using System;
+
+public static class Kata
+{
+  public static string Likes(string[] name)
+  {
+    switch (name.Length)
+    {
+      case 0: 
+      {
+        return "no one likes this";
+      }
+      case 1:
+      {
+        return $"{name[0]} likes this";
+      }
+      case 2:
+      {
+        return $"{name[0]} and {name[1]} like this";
+      }
+      case 3:
+      {
+        return $"{name[0]}, {name[1]} and {name[2]} like this";
+      }
+      default:
+      {
+        int count = name.Length - 2;
+        return $"{name[0]}, {name[1]} and {count} others like this";
+      }
+    }
+  }
+}
